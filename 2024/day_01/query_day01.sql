0c546308e3c4e753926b6d1bfefc7f64dee91c8a
Daniel Roelfs <daniel@roelfs.net>
2024-12-01 19:20:13 +0100

    new instructions

--- 2024/day_01/query_day01.sql
+++ 2024/day_01/query_day01.sql
@@ -6,12 +6,26 @@ WITH latest_list AS (
     GROUP BY child_id
 ),
 
+fav_colors AS (
+    SELECT
+        wl.child_id,
+        wl.wishes::json ->> 'colors' AS color_array
+    FROM wish_lists AS wl
+    INNER JOIN latest_list AS ll ON
+        wl.child_id = ll.child_id
+        AND wl.submitted_date = ll.latest
+),
+
 wishes AS (
     SELECT
         wl.child_id,
         wl.wishes::json ->> 'first_choice' AS primary_wish,
-        wl.wishes::json ->> 'second_choice' AS backup_wish
+        wl.wishes::json ->> 'second_choice' AS backup_wish,
+        (wl.wishes::json -> 'colors')::json ->> 0 AS favorite_color,
+        JSON_ARRAY_LENGTH(fc.color_array::json) AS n_colors
     FROM wish_lists AS wl
+    INNER JOIN fav_colors AS fc
+        ON wl.child_id = fc.child_id
     INNER JOIN latest_list AS ll ON
         wl.child_id = ll.child_id
         AND wl.submitted_date = ll.latest
@@ -37,10 +51,12 @@ SELECT
     ch.name,
     wsh.primary_wish,
     wsh.backup_wish,
+    wsh.favorite_color,
+    wsh.n_colors,
     cmplx.gift_complexity,
     cmplx.workshop_assignment
 FROM children AS ch
 INNER JOIN wishes AS wsh ON ch.child_id = wsh.child_id
 INNER JOIN complexity AS cmplx ON wsh.primary_wish = cmplx.toy_name
 ORDER BY ch.name
-LIMIT 5;
+LIMIT 10;
